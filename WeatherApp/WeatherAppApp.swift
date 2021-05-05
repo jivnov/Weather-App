dd808e525e89065f43bfb33ca6e1e9261108bd2c
Andrei Zhyunou <jivnov27@gmail.com>
2021-05-05 12:42:25 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Andrei Zhyunou on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
